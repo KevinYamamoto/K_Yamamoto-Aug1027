f8e96fc343919250726e911602424f5e4c207335
KevinYamamoto <18yamamotok55@millshighsch.org>
2017-08-24 09:43:07 -0700

    trying again

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,12 @@
+/*Kevin Yamamoto
+ * August 23,2017
+ * Project 2 -- Use print and println
+ */
+public class Project_2 {
+
+	public static void main(String[] args) {
+		System.out.print("Greetings!");
+		System.out.println("Hi There!");
+		
+	}
+}
